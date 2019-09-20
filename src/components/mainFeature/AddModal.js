d2f80d8a795f044c8c416dd6d49b8a051af19f87
Noah Bartfield <noahbartfield@Noahs-MacBook-Pro.local>
2019-09-20 11:41:36 -0500

    ready for soft demo day

--- src/components/mainFeature/AddModal.js
+++ src/components/mainFeature/AddModal.js
@@ -5,12 +5,16 @@ import { Button, Modal } from 'semantic-ui-react'
 class AddModal extends Component {
 
     render() {
+        console.log(this.props.artist)
         const lyricArray = this.props.lyrics.split(/(?=[A-H]|[J-Z])/)
         return (
-            <Modal onClose={this.props.close} onOpen={this.props.open} open={this.props.showModal} trigger={<Button>View Results</Button>} closeIcon>
+            <Modal onClose={this.props.close} onOpen={this.props.open} open={this.props.showModal} trigger={<Button className="tiny grey">View Results</Button>} closeIcon>
                 <Modal.Header className="modalHeader">{this.props.title.split('(')[0]}</Modal.Header>
                 <div className="lyricsAndComments">
                     <Modal.Content className="lyricsInModal">
+                        {this.props.artist !== "" &&
+                            <h5 className="artistName">{this.props.artist}</h5>
+                        }
                         {this.props.lyrics !== "" &&
                         lyricArray.map(lyric => {
                             return <p key={Math.random() * 9999999999999}>{lyric.replace("[", "").replace("]", "").replace("Verse", "").replace("Chorus", "").replace("Bridge", "").replace("Outro", "").replace(/[1-9]/, "")}</p>
@@ -23,7 +27,7 @@ class AddModal extends Component {
                         <textarea className="textArea" rows="20" cols="15" id="comments" onChange={this.props.handleFieldChange} value={this.props.comments}></textarea>
                     </Modal.Content>
                 </div>
-                <Button color="red" attached onClick={this.props.addSong}>Save</Button>
+                <Button className="saveButton" attached onClick={this.props.addSong}>Save</Button>
             </Modal>
         )
     }
